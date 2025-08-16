6c224ce8c3903cdc0f71a5f0eca38fd9d476e242
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-16 14:28:42 +0100

    prep for pip

--- hamplots/__init__.py
+++ hamplots/__init__.py
@@ -1,13 +1,15 @@
 """
     Standard Python initialiser handling imports from modules and version number
 """
-from .datasources import *
-from .analysers import *
 
 from importlib.metadata import version
 try:
     __version__ = version("ham-plots")
 except:
     __version__ = ""
-print(f"\nHamplots {__version__} by Dr Alan Robinson G1OJS\n\n")
+
+__all__ = ["aggregator", "plotter", "datasources"]
+
+if __name__ == "__main__":
+    print(f"\nHamplots {__version__} by Dr Alan Robinson G1OJS\n\n")
 
